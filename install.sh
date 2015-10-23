2fc3e6ed381df3cf092230045ca8fe58f0c6822c
flames of love <mechanicofthesequence@gmail.com>
2015-10-23 04:51:13 +0200

    submodule init

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 node-pre-gyp install 2> /dev/null || \
-	([ -d "libusb" ] || git clone --depth=1 https://github.com/kevinmehall/libusb.git) && \
+	git submodule update --init --recursive && \
 	node-pre-gyp rebuild
